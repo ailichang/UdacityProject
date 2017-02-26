0bd7c5f0eaa6e87f4e226b64c47058f1ddf93f64
Emily Chang <emilyailichang@gmail.com>
2017-02-26 00:26:24 -0800

    reformat resume

--- frontend-nanodegree-resume-master/js/helper.js
+++ frontend-nanodegree-resume-master/js/helper.js
@@ -12,22 +12,22 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
-var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderName = '<h1 id="name">%data%</h1><br>';
+var HTMLheaderRole = '<span class="orange-text">%data%</span><br><br>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item center-text"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item center-text" ><span class="orange-text fontawesome-phone"></span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item center-text"><span class="orange-text fontawesome-envelope-alt"></span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item center-text"><span class="orange-text fontawesome-twitter"></span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item center-text"><span class="orange-text fontawesome-github"></span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item center-text"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item center-text"><span class="orange-text fontawesome-map-marker"></span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
-var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLwelcomeMsg = '<h2>%data%</h2>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item center-text"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
@@ -87,6 +87,7 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+   logClicks(loc.screenX, loc.screenY);
 });
 
 
